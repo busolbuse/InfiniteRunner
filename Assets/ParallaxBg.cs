3e4dbfc6c6d71bb0610e9bfc12ca18cde093102b
Buse Berren <bunal9373@gmail.com>
2025-02-05 17:35:49 +0300

    ParallaxBG Creating
    a constantly scrolling background.

---
+++ Assets/ParallaxBg.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxBg : MonoBehaviour
+{
+    Player player;
+
+    public float depth = 1; //same depth as the player
+
+    private void Awake()
+    {
+        player = GameObject.Find("Player").GetComponent<Player>();
+    }
+
+    void Start()
+    {
+        
+    }
+
+    
+    void FixedUpdate()
+    {
+        float realVelocity = player.velocity.x / depth;// the greater the depth, the smaller the speed
+        Vector2 position = transform.position;  
+
+        position.x -= realVelocity * Time.fixedDeltaTime; // opposite direction of the player
+        
+        if(position.x <= -10 )
+        {
+            position.x = 10;
+        }
+        
+        transform.position = position;
+
+    }
+}
