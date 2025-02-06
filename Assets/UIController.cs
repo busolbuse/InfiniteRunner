e6048eb4279dcffb23e15a2897f7459d6d17a28f
Buse Berren <bunal9373@gmail.com>
2025-02-07 00:22:56 +0300

    Dead,Results and Menu panel.
    Player dies when hitting the wall and the results panel comes up, can be replayed or go back to the menu.

--- Assets/UIController.cs
+++ Assets/UIController.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 
 public class UIController : MonoBehaviour
@@ -9,12 +10,18 @@ public class UIController : MonoBehaviour
     Player player;
     TextMeshProUGUI distanceText;
 
+    //results pannel
+    GameObject results;
+    TextMeshProUGUI finalDistanceText;
+
     private void Awake()
     {
         player = GameObject.Find("Player").GetComponent<Player>();
         distanceText = GameObject.Find("DistanceText").GetComponent<TextMeshProUGUI>();
 
-
+        finalDistanceText = GameObject.Find("FinalDistance").GetComponent<TextMeshProUGUI>();
+        results = GameObject.Find("Results");
+        results.SetActive(false);//default
     }
 
 
@@ -27,6 +34,28 @@ public class UIController : MonoBehaviour
     void Update()
     {
         int distance = Mathf.FloorToInt(player.distance);
-        distanceText.text = distance + "m";
+        distanceText.text = distance + " m";
+        
+        //if player dead
+        if(player.isDead)
+        {
+            results.SetActive(true);//show results pannel
+            finalDistanceText.text = distance + " m";
+        }
     }
+
+    //Scenes and Button
+    public void Quit()
+    {
+        //use scene management
+        SceneManager.LoadScene("RanMenu");
+    }
+
+    public void Retry()
+    {
+        //use scene management
+        SceneManager.LoadScene("SampleScene");//default scene( current  scene)
+    }
+
+
 }
